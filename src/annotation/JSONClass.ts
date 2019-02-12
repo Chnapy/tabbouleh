2b36fd4615945e8b6c1791772ea1dead2b5a310b
Richard <richardhaddad@hotmail.fr>
2019-02-12 22:57:47 +0100

    feat: I see the light

--- src/annotation/JSONClass.ts
+++ src/annotation/JSONClass.ts
@@ -3,10 +3,10 @@ import { REFLECT_JSON_SCHEMA } from './ReflectKeys'
 import { Optional } from '../jsonTypes/Optional'
 import { JSONRoot } from '../jsonTypes/JSONTypes'
 
-export default function JSONSchema<T extends { new(): object } & any>(value: Optional<JSONRoot> = {}) {
-
+export default function JSONSchema<T extends { new (): object } & any>(
+  value: Optional<JSONRoot> = {}
+) {
   return (target: T) => {
-
     console.log('class', target)
 
     // const name = target.name
@@ -15,14 +15,12 @@ export default function JSONSchema<T extends { new(): object } & any>(value: Opt
       type: 'object',
       $schema: 'todo',
       ...value
-    };
+    }
 
-    Reflect.defineMetadata(REFLECT_JSON_SCHEMA, json, target);
+    Reflect.defineMetadata(REFLECT_JSON_SCHEMA, json, target)
 
     // Reflect.defineMetadata(REFLECT_JSON_OPTIONS, {}, target)
 
-    console.log('class - metadata', Reflect.getMetadata(REFLECT_JSON_SCHEMA, target));
-
+    console.log('class - metadata', Reflect.getMetadata(REFLECT_JSON_SCHEMA, target))
   }
-
 }
