86748218699a2f31a6a91c4217c41a7be00bbcaf
Richard <richardhaddad@hotmail.fr>
2019-02-19 23:10:45 +0100

    fix export from tabbouleh.ts

--- src/tabbouleh.ts
+++ src/tabbouleh.ts
@@ -1,3 +1,12 @@
 import Tabbouleh from './engine/Tabbouleh';
 
+export * from './annotation/JSONSchema';
+export * from './annotation/JSONProperty';
+
+export * from './types/JSONTypes';
+export * from './types/ClassTypes';
+export * from './types/UtilTypes';
+
+export * from './exception/NotAJsonSchemaError';
+
 export default Tabbouleh;
