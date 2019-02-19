607fa47ae4258cf2e1f32f4e178a6b7fa51cd5d3
Richard <richardhaddad@hotmail.fr>
2019-02-19 22:56:15 +0100

    refactor: refactor of types names, and readme (#10)
    BREAKING CHANGE: types rename

--- src/exception/NotAJsonSchemaError.ts
+++ src/exception/NotAJsonSchemaError.ts
@@ -3,12 +3,10 @@ import { ClassLike } from '../types/ClassTypes';
 const msg = (name: string) => `Class called by Tabbouleh but not annotate with @JSONClass: ${name}`;
 
 export default class NotAJsonClassError extends Error {
-
   constructor(target: ClassLike) {
     super(msg(target.name));
     Error.call(this);
     Error.captureStackTrace(this, this.constructor);
     Object.setPrototypeOf(this, NotAJsonClassError.prototype);
   }
-
 }
