1823c89dd92725dde4a4c38da3c8be8fbefd3f1c
Richard Haddad <richard.haddad.externe@cnp.fr>
2019-02-20 17:00:04 +0100

    refactor JSONProperty

--- src/annotation/JSONProperty.ts
+++ src/annotation/JSONProperty.ts
@@ -4,14 +4,33 @@ import AnnotationEngine from '../engine/AnnotationEngine';
 import { ClassLike } from '../types/ClassTypes';
 import FunctionAnnotation from "./FunctionAnnotation";
 
-export function JSONProperty<J extends JSONEntity<any, any>>(value: Optional<J> = {}): Function {
-  return <T extends ClassLike>(
-    prototype: T['prototype'],
-    key: keyof T['prototype'] & string,
-    descriptor?: PropertyDescriptor
-  ): void => {
-    AnnotationEngine.defineReflectProperties(prototype, key, value);
+export function JSONProperty(prototype: any, key: string, descriptor?: PropertyDescriptor): void;
+export function JSONProperty<J extends JSONEntity<any, any>>(value: Optional<J>): Function;
+export function JSONProperty<J extends JSONEntity<any, any>>(...args: any[]): Function | void {
+
+  const compute = (value: Optional<J> = {}) => {
+
+    return <C extends ClassLike>(
+      prototype: C['prototype'],
+      key: keyof C['prototype'] & string,
+      descriptor?: PropertyDescriptor
+    ): void => {
+      AnnotationEngine.defineReflectProperties(prototype, key, value);
+    };
   };
+
+  if(args.length === 1) {
+
+    const value = args[0];
+
+    return compute(value);
+  } else {
+
+    const prototype = args[0];
+    const key = args[1];
+
+    compute()(prototype, key);
+  }
 }
 
 export function JSONString(value: Optional<JSONEntityString> = {}) {
